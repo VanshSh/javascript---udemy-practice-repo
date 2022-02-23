cba52d880e5f64804ef07c75c84d41fa48144ddd
VanshSh <vanshsharma9354@gmail.com>
2022-02-23 12:04:05 +0530

    Learnt Babel and Parcel

---
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -0,0 +1,17 @@
+// import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+
+// const cart = {
+//   items: [
+//     { product: 'bread', price: 10 },
+//     { product: 'Milk', price: 5 },
+//   ],
+//   user: {
+//     loggedIn: true,
+//   },
+// };
+
+// const cartCopy = cloneDeep(cart);
+
+// cartCopy.user.loggedIn = false;
+// console.log(cartCopy);
+// console.log("Hello World")
